5d1ad96ae7ac374bc40f7461524d969353e9ced3
Replit user <>
2022-09-05 09:56:02 +0000

    Added rate-limiter and proper status-codes

--- howold.route.js
+++ howold.route.js
@@ -2,33 +2,48 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', async (req, res, next) => {
+  // get query string dob
+  const dob = req.query.dob;
+  // make sure dob paramenter exists
+  if (!dob) {
+    return res.status(400).json({
+      message: '"dob" parameter is not properly used',
+      error: true
+    })
+  }
 
-    try {
-        // get query string dob
-        const dob = req.query.dob;
+  try {
 
-        // get today's date
-        const todaysDate = new Date();
-        // get date of birth from query string timestamp
-        const dateOfBirth = new Date(dob);
-        let age = todaysDate.getFullYear() - dateOfBirth.getFullYear();
-        const month = todaysDate.getMonth() - dateOfBirth.getMonth();
-        if (month < 0 || (month === 0 && todaysDate.getDate() < dateOfBirth.getDate())) {
-            age--;
-        }
-
-        return res.json({
-            age: age,
-            error: false
-        })
-
-    } catch (err) {
-        console.log(err);
-        return res.json({
-            message: 'An error occured',
-            error: true
-        })
+    // get today's date
+    const todaysDate = new Date();
+    // get date of birth from query string timestamp
+    const dateOfBirth = new Date(dob);
+    // make sure dob is a valid date string
+    if (!(dateOfBirth instanceof Date && !isNaN(dateOfBirth))) {
+      return res.status(422).json({
+        message: '"dob" parameter value is invalid',
+        error: true
+      })
     }
+
+    let age = todaysDate.getFullYear() - dateOfBirth.getFullYear();
+    const month = todaysDate.getMonth() - dateOfBirth.getMonth();
+    if (month < 0 || (month === 0 && todaysDate.getDate() < dateOfBirth.getDate())) {
+      age--;
+    }
+
+    return res.json({
+      age: age,
+      error: false
+    })
+
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      message: 'An error occured',
+      error: true
+    })
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const HowOldRoute = require('./howold.route');
-const app = express();
 
-app.use('/howold', HowOldRoute);
+const rateLimiter = rateLimit({
+  windowMs: 1000,
+  max: 3,
+  message: 'You have reached maximum request',
+  statusCode: 429
+});
+
+
+const app = express();
+app.use('/howold', rateLimiter, HowOldRoute);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
