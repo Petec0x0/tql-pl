dbd7ab433971343e93d345ee31afd0f48bc2cda2
Replit user <>
2022-09-17 05:59:28 +0000

    Break the test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const HowOldRoute = require('./howold.route');
 
 const rateLimiter = rateLimit({
-  windowMs: 230,
+  windowMs: 210,
   max: 3,
   message: 'You have reached maximum request',
   statusCode: 429
