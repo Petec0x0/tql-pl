0c906563923b961869d2bc4ecc9483f30d0862a2
Replit user <>
2022-09-18 13:23:42 +0000

    ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const HowOldRoute = require('./howold.route');
 
-const rateLimiter = rateLimit({
-  windowMs: 1000,
-  max: 3,
-  standardHeaders: true
-});
+// const rateLimiter = rateLimit({
+//   windowMs: 1000,
+//   max: 3,
+//   standardHeaders: true
+// });
 
 
 const app = express();
 // Apply the rate limiting middleware to all requests
-app.use(rateLimiter);
+app.use(rateLimit({
+    windowMs: 1000,
+    max: 3,
+    standardHeaders: true
+}));
+
 app.use('/howold', HowOldRoute);
 
 // set port, listen for requests
